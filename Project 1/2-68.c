5b224705b6de60dda51eca669f0be04f41a0ec7b
Justin Robles <j.70.robles@gmail.com>
2017-10-02 11:30:06 -0700

    finished 2-68 w/ comments

--- Project 1/2-68.c
+++ Project 1/2-68.c
@@ -1,7 +1,28 @@
 //
 // Created by Justin on 9/28/2017.
 //
+#include <stdio.h>
+
+int lower_on_mask(int n){
+    // For the case where n = w you will get an error when shifting a w-bit word w-times. In the book it tells you that
+    // to get a w-bit word with 8 1's in the least significant bits you use (1 << 8) - 1
+    // 1 == 0001
+    // 1 << 8 == 0001 0000 0000
+    // (1 << 8) - 1 == 1111 1111
+    // to make this work with (n-1) to get a 32-bit mask with 32 1's you must start with 2 instead of 1
+    // 2 == 0010
+    // (2 << 31) == 0001 0000 0000 0000 0000
+    // (2 << 31) - 1  == 1111 1111 1111 1111
+
+    signed mask = (2 << (n-1)) - 1;
+    printf("mask shifted %i spots: %X\n", n, mask);
+    return mask;
+}
 
 int main() {
-
+    lower_on_mask(1);
+    lower_on_mask(2);
+    lower_on_mask(3);
+    lower_on_mask(5);
+    lower_on_mask(32);
 }
