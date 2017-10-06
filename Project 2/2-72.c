d7407d4e331e0c9884a1d70e886a3ddf459e07b3
Justin Robles <j.70.robles@gmail.com>
2017-10-06 09:20:01 -0700

    Working on 2-83 still

--- Project 2/2-72.c
+++ Project 2/2-72.c
@@ -2,3 +2,39 @@
 // Created by Justin on 10/4/2017.
 //
 
+/* Copy integer into buffer if space is available */
+#include <mem.h>
+#include <stdio.h>
+
+/* WARNING: The following code is buggy */
+void copy_intOLD(int val, void *buf, int maxbytes) {
+    if (maxbytes-sizeof(val) >= 0) {
+        memcpy(buf, (void *) &val, sizeof(val));
+        // If this print statement prints then that means there is a bug in the code since I made the
+        // maxbytes = 0
+        printf("copy_intOLD: Bug");
+    }
+}
+
+void copy_int(int val, void *buf, int maxbytes) {
+    // Changing this to a comparison fixes the casting issue from before and allows this code to run
+    // properly. "copy_intOLD: Bug" will print but "copy_int: Bug" will not
+    if (maxbytes >= sizeof(val)) {
+        memcpy(buf, (void *) &val, sizeof(val));
+        printf("copy_int: Bug");
+    }
+}
+
+main(){
+    int a = 10;
+    int* buf = &a;
+    copy_intOLD(1, buf, 0);
+    copy_int(1, buf, 0);
+}
+
+// A: This does not work because ints are signed by default but the value of size_t is
+//    unsigned since it will never be a negative value. So in this case the unsigned value
+//    casts the signed value to an unsigned int as well.
+
+// B: we could correct this by comparing maxbytes to sizeof(val) rather than subtracting
+//
--- Project 2/2-83.c
+++ Project 2/2-83.c
@@ -1,4 +1,40 @@
 //
 // Created by Justin on 10/4/2017.
 //
+#include <stdio.h>
 
+// Use the following code for f2u:
+unsigned f2u(float f) {
+    return *((unsigned*)&f);
+}
+
+int float_le(float x, float y) {
+    unsigned ux = f2u(x);
+    unsigned uy = f2u(y);
+    printf("%f %f\n", (x,y));
+    printf("%x %x\n", (x,y));
+    printf("%x %x\n", (ux,uy));
+    /* Get the sign bits */
+    unsigned sx = ux >> 31;
+    unsigned sy = uy >> 31;
+    printf("%x %x\n", (sx,sy));
+
+    /* Give an expression using only ux, uy, sx, and sy */
+    // there are a few cases we must consider:
+    // 1: That x is negative and y is positive
+    // 2: That x and y have the same sign (+ or -)
+    //      2a: x is smaller
+    //      2b: y is smaller
+    // so in case 1
+    return (sx > sy) | (ux < uy);
+}
+
+main() {
+    float x = 1;
+    float y = 2;
+    if (float_le(x, y)){
+        printf("TRUE");
+    } else {
+        printf("FALSE");
+    }
+}
